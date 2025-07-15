a03b03a49c8959b179be8de07fafd5a9e6bee9e6
Alimyustore <yusuftajudeenajibola@gmail.com>
2025-07-15 14:42:15 +0100

    switch case

---
+++ src/app/switchcase/switchcase.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-switchcase',
+  imports: [],
+  templateUrl: './switchcase.html',
+  styleUrl: './switchcase.css'
+})
+export class Switchcase {
+
+}
