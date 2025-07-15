4f9b0a5eb504842c7b1b469e94bafec000db53fb
Alimyustore <yusuftajudeenajibola@gmail.com>
2025-07-15 15:11:08 +0100

    done with switch case

--- src/app/component/component.ts
+++ src/app/component/component.ts
@@ -5,10 +5,11 @@ import { Button } from '../button/button';
 import { Counter } from '../counter/counter';
 import { Get } from '../get/get';
 import { Ifelse } from '../ifelse/ifelse';
+import { Switchcase } from '../switchcase/switchcase';
 
 @Component({
   selector: 'component',
-  imports: [Login, Signup, Button, Counter, Get, Ifelse],
+  imports: [Login, Signup, Button, Counter, Get, Ifelse, Switchcase],
   templateUrl: './component.html',
   styleUrl: './component.css'
 })
--- src/app/switchcase/switchcase.ts
+++ src/app/switchcase/switchcase.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-switchcase',
+  selector: 'switchcase',
   imports: [],
   templateUrl: './switchcase.html',
   styleUrl: './switchcase.css'
 })
 export class Switchcase {
+color : string = ""
+handleColor(val: string){
+  this.color=val
+}
 
+changeColor(event: Event){
+  this.color = (event.target as HTMLInputElement).value
+}
 }
