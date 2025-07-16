212945647141b37db17bcfab7304c8932134dcfa
Alimyustore <yusuftajudeenajibola@gmail.com>
2025-07-16 15:48:06 +0100

    To do list with angular

--- src/app/to-do/to-do.ts
+++ src/app/to-do/to-do.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'to-do',
-  imports: [],
+  selector: 'to-dos',
+  imports: [FormsModule],
   templateUrl: './to-do.html',
-  styleUrl: './to-do.css'
+  styleUrl: './to-do.css',
 })
 export class ToDo {
+  task: string = '';
+  taskList: { id: number; name: string }[] = [];
 
+  addTask() {
+    this.taskList.push({ id: this.taskList.length + 1, name: this.task });
+    this.task = ""
+  };
+  handleDelete(id:number){
+    this.taskList= this.taskList.filter((item)=> item.id !== id)
+  };
 }
