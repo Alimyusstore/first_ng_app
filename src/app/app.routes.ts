8ac4de474e9d26b0a822f802d60bbe1961169a06
Alimyustore <yusuftajudeenajibola@gmail.com>
2025-07-17 08:15:26 +0100

    Basic routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { Login } from './login/login';
+import { About } from './about/about';
+import { Contact } from './contact/contact';
+import { Signup } from './signup/signup';
+import { Counter } from './counter/counter';
+import { ToDo } from './to-do/to-do';
+import { Ifelse } from './ifelse/ifelse';
+import { Switchcase } from './switchcase/switchcase';
+import { Components } from './component/component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'login', component: Login},
+    {path: 'about', component: About},
+    {path: 'signup', component: Signup},
+    {path: 'to-do', component: ToDo},
+    {path: 'counter', component: Counter},
+    {path: 'ifelse', component: Ifelse},
+    {path: 'switchcase', component: Switchcase},
+    {path: 'contact', component: Contact},
+    {path: 'component', component: Components},
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { Components } from './component/component';
-import { Login } from './login/login';
+import { RouterLink, RouterOutlet } from '@angular/router';
+// import { Components } from './component/component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Components],
+  imports: [RouterOutlet, 
+    // Components,
+     RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
